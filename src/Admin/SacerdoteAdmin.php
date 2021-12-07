51c19ca6d1a8e62d8e151db3f0585b4fab6f7472
karlos <karlosgares@gmail.com>
2021-12-07 17:11:48 +0100

    Filtramos los sacerdotes por activos

--- src/Admin/SacerdoteAdmin.php
+++ src/Admin/SacerdoteAdmin.php
@@ -1,15 +1,39 @@
 <?php
-namespace App\Admin;
 
+namespace App\Admin;
 
 class SacerdoteAdmin extends UsuarioAdmin
 {
-	protected $baseRouteName = 'sacerdote';
+    protected $baseRouteName = 'sacerdote';
     protected $baseRoutePattern = 'sacerdote';
-	
-	public function getTipo() { return 0;}
 
-	public function getMinTime() { return '07:00:00'; }
-    public function getMaxTime() { return '23:00:00'; }
-    public function getHeaderRight() { return 'prev,next'; }
+    public function getTipo()
+    {
+        return 0;
+    }
+
+    public function createQuery($context = 'list')
+    {
+        $query = parent::createQuery($context);
+        $parameters = $this->getFilterParameters();
+        list($tableAlias) = $query->getQueryBuilder()->getRootAliases();
+        if (!array_key_exists("activo", $parameters)) {
+            $query->where($tableAlias . '.activo = :activo')->setParameter('activo', 1);
+        }
+
+        return $query;
+    }
+
+    public function getMinTime()
+    {
+        return '07:00:00';
+    }
+    public function getMaxTime()
+    {
+        return '23:00:00';
+    }
+    public function getHeaderRight()
+    {
+        return 'prev,next';
+    }
 }
