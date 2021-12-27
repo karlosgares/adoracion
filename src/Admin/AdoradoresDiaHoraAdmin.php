2fdf5016b3f32bcc3cc9237b10a1f11a7e83b484
karlos <karlosgares@gmail.com>
2021-12-27 11:04:18 +0100

    Corregimos los array de días toString día y hora

--- src/Admin/AdoradoresDiaHoraAdmin.php
+++ src/Admin/AdoradoresDiaHoraAdmin.php
@@ -3,21 +3,30 @@
 namespace App\Admin;
 
 use App\Entity\AdoradoresDiaHora;
-use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
-use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 final class AdoradoresDiaHoraAdmin extends AbstractAdmin
 {
 
+    public function toString($entity) {
+        if ($entity->getId() > 0) {
+            return sprintf("%s %s:00", $entity->getDiaText(), $entity->getHoraText());
+        }
+
+
+        return "Nueva";
+    }
+
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('dia', null, [], ChoiceType::class, ['choices' => AdoradoresDiaHora::$diasSemana])
+            ->add('dia', null, [], ChoiceType::class, ['choices' => array_flip(AdoradoresDiaHora::$diasSemana)])
             ->add('hora', null, [], ChoiceType::class, ['choices' => range(0, 23)])
             ->add('numero')
         ;
@@ -26,8 +35,8 @@ final class AdoradoresDiaHoraAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->add('diaText',null, ["label"=> "Día"])
-            ->add('horaText',null, ["label"=> "Hora"])
+            ->add('diaText', null, ["label" => "Día"])
+            ->add('horaText', null, ["label" => "Hora"])
             ->add('numero')
             ->add('_action', null, [
                 'actions' => [
@@ -41,7 +50,7 @@ final class AdoradoresDiaHoraAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('dia', ChoiceType::class, ['choices' => AdoradoresDiaHora::$diasSemana])
+            ->add('dia', ChoiceType::class, ['choices' => array_flip(AdoradoresDiaHora::$diasSemana)])
             ->add('hora', ChoiceType::class, ['choices' => range(0, 23)])
             ->add('numero')
         ;
@@ -56,7 +65,6 @@ final class AdoradoresDiaHoraAdmin extends AbstractAdmin
             ->add('numero')
         ;
     }
-
 
     protected function configureRoutes(RouteCollection $collection)
     {
